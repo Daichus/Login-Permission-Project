6adb16d5b32a61381c2850e3dea834bde13be339
D1204493 <161418128+D1204493@users.noreply.github.com>
2024-11-02 22:52:55 +0800

    新增關於Position查詢功能
    1. Repository
    2. Model
    3. Service
    4. Controller
    都有修改

---
+++ demo/src/main/java/login/permission/project/classes/model/PositionDTO.java
@@ -0,0 +1,16 @@
+package login.permission.project.classes.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PositionDTO {
+    private int position_id;
+    private String position;
+    private int unit_id;
+    private String unit_name;
+}
