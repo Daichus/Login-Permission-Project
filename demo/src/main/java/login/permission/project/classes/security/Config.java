144763623c62b804d0caa969d943fdcb1820232c
Daichus <154423666+Daichus@users.noreply.github.com>
2024-11-27 11:27:01 +0800

    add securityConfg

---
+++ demo/src/main/java/login/permission/project/classes/security/Config.java
@@ -0,0 +1,22 @@
+package login.permission.project.classes.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/login").permitAll()
+
+                );
+
+
+        return http.build();
+    }
+}
