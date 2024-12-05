1c7ddb75b20af61a09a89b286ff84b98e76b5828
D1204493 <161418128+D1204493@users.noreply.github.com>
2024-12-06 04:39:15 +0800

    LoginRecordController 加上權限控制
    permission_code的控制

--- demo/src/main/java/login/permission/project/classes/controller/LoginRecordController.java
+++ demo/src/main/java/login/permission/project/classes/controller/LoginRecordController.java
@@ -5,6 +5,7 @@ import login.permission.project.classes.model.LoginRecord;
 import login.permission.project.classes.service.LoginRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,29 +20,34 @@ public class LoginRecordController {
 
     // 獲取所有登入記錄
     @GetMapping("/get")
+    @PreAuthorize("hasAuthority('login_rec_read')")
     public List<LoginRecord> getAllLoginRecords() {
         return lrs.getAllLoginRecords();
     }
 
     // 新增登入記錄
     @PostMapping("/add")
+    @PreAuthorize("hasAuthority('login_rec_create')")
     public String addLoginRecord(@RequestBody LoginRecord loginRecord) {
         return lrs.addLoginRecord(loginRecord);
     }
 
     // 修改登入記錄
     @PutMapping("/edit")
+    @PreAuthorize("hasAuthority('login_rec_update')")
     public String updateLoginRecord(@RequestBody LoginRecord loginRecord) {
         return lrs.updateLoginRecord(loginRecord);
     }
 
     // 刪除登入記錄
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('login_rec_delete')")
     public String deleteLoginRecord(@PathVariable int id) {
         return lrs.deleteLoginRecord(id);
     }
 
     @PostMapping("/getLoginRecord")
+    @PreAuthorize("hasAuthority('login_rec_create','login_rec_read')")
     public ResponseEntity<?> getRecordByPermissionId(HttpServletRequest request) {
         return lrs.getRecordByPermissionCode(request);
     }
